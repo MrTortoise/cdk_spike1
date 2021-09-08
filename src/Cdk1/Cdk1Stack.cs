cbf983d108d628ed87b46bf39d73b12c1ca321b5
John Nicholas <john.nicholas@dinoroar.io>
2021-09-08 21:21:34 +0100

    Initial commit

---
+++ src/Cdk1/Cdk1Stack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace Cdk1
+{
+    public class Cdk1Stack : Stack
+    {
+        internal Cdk1Stack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
